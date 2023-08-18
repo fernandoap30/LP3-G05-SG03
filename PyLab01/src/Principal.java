ab34c5fe7aa621d5b04488a85e876ef21cc97b69
fernando <fernando.apmarce22@gmail.com>
2023-08-18 18:50:24 -0500

    las vidas van en forma descendente ahora

--- PyLab01/src/Principal.java
+++ PyLab01/src/Principal.java
@@ -4,15 +4,15 @@ public class Principal {
 	public static void main(String[] args) {
 		int vida, rp;
 		boolean fl = false;
-		vida = 0;
+		vida = 4;
 		Scanner sc = new Scanner(System.in);
 		do{
-			vida++;
+			vida--;
 			System.out.println("vida: "+ vida + "\t Cual es el numero secreto?");
 			rp = sc.nextInt();
 			if (rp == 1234)
 				fl = true;
-		}while(vida < 3 && !fl);
+		}while(vida > 1 && !fl);
 		if (fl ==true)
 			System.out.println("Adivinaste!!!!");
 		else
