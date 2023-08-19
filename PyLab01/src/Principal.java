95d5dcd11609d7d523896586a78d6fdf43efd668
fernando <fernando.apmarce22@gmail.com>
2023-08-18 21:14:40 -0500

    aumento de vidas y cambio en el numero secreto

--- PyLab01/src/Principal.java
+++ PyLab01/src/Principal.java
@@ -4,13 +4,13 @@ public class Principal {
 	public static void main(String[] args) {
 		int vida, rp;
 		boolean fl = false;
-		vida = 4;
+		vida = 6;
 		Scanner sc = new Scanner(System.in);
 		do{
 			vida--;
 			System.out.println("vida: "+ vida + "\t Cual es el numero secreto?");
 			rp = sc.nextInt();
-			if (rp == 1234)
+			if (rp == 9876)
 				fl = true;
 		}while(vida > 1 && !fl);
 		if (fl ==true)
